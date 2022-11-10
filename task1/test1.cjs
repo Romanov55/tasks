1c970103993e83b6517bc94fea4d2a031158a6bd
Romanov55 <Romanov20let@gmail.com>
2022-11-10 14:32:57 +0600

    up test1

--- task1/test1.cjs
+++ task1/test1.cjs
@@ -1,25 +1,29 @@
-var fs = require('fs');
-var array = fs.readFileSync('task1/file.txt').toString().split("\n");
-let sum = 0;
-let result = [];
-let result2 = [];
-for (let item of array) {
-    let b = item.split('')
-    const [first, ...rest] = b;
-    const rt = Number(rest.join(''))
+const fs = require('fs');
+
+const array = fs.readFileSync('/home/vladislav/tasks/task1/file.txt').toString().split('\n');
+
+const test = (array) => {
+  let sum = 0;
+  const result = [];
+  const result2 = [];
+  for (const item of array) {
+    const b = item.split('');
+    const [, ...rest] = b;
+    const rt = Number(rest.join(''));
     if (item[0] === '+') {
-        sum += rt
+      sum += rt;
+    } else if (item[0] === '-') {
+      sum -= rt;
     }
-    else if (item[0] === '-') {
-        sum -= rt
-    };
 
-    if (!result.includes(sum)){
-        result.push(sum)
+    if (!result.includes(sum)) {
+      result.push(sum);
+    } else {
+      result2.push(sum);
     }
-    else {
-        result2.push(sum)
-    };
-};
+  }
 
-console.log(result2[0])
+  return result2[0];
+}
+
+test(array)
