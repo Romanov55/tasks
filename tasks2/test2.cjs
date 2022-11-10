c537c1a35a43ba2bcbd28d30f47a301a01b75072
Romanov55 <Romanov20let@gmail.com>
2022-11-10 14:34:31 +0600

    up test2

--- tasks2/test2.cjs
+++ tasks2/test2.cjs
@@ -1,40 +1,50 @@
-var fs = require('fs');
-var array = fs.readFileSync('tasks2/file2.txt').toString().split("\n");
+const fs = require('fs');
+
+const array = fs.readFileSync('tasks2/file2.txt').toString().split('\n');
 
 const vovels = (item) => {
-    let sum = 0;
-    for (let i = 0; i < item.length; i += 1) {
-        if ('aeiou'.includes(item[i])) {
-            sum += 1
-        }
+  let sum = 0;
+  for (let i = 0; i < item.length; i += 1) {
+    if ('aeiou'.includes(item[i])) {
+      sum += 1;
     }
-    
-    if (sum > 2) {
-        return true;
-    }
-}
+  }
+
+  if (sum > 2) {
+    return true;
+  }
+
+  return false;
+};
 
 const exception = (item) => {
-    if (!item.includes('ab') && !item.includes('cd') && !item.includes('pq') && !item.includes('xy')) {
-        return true
-    }
-}
+  if (!item.includes('ab') && !item.includes('cd') && !item.includes('pq') && !item.includes('xy')) {
+    return true;
+  }
+
+  return false;
+};
 
 const aa = (item) => {
-    for (let j = 0; j < item.length; j += 1){
-        if (item[j] === item[j-1] ) {
-            return true
-        }
+  for (let j = 0; j < item.length; j += 1) {
+    if (item[j] === item[j - 1]) {
+      return true;
     }
-}
+    else {
+      return false;
+    }
+  }
+};
 
-
-
-let result = 0
-for (let item of array) {
+const test = (array) => {
+  let result = 0;
+  for (const item of array) {
     if (vovels(item) && exception(item) && aa(item)) {
-        result +=1
+      result += 1;
     }
+  }
+
+  return result;
 }
 
-console.log(result)
+test(array)
